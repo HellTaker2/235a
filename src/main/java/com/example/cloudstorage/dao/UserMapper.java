60db18f7e6d48aae4229c1b13028e7674ea6111c
zsr <649322117@qq.com>
2025-05-20 21:15:01 +0800

    zsr

--- src/main/java/com/example/cloudstorage/dao/UserMapper.java
+++ src/main/java/com/example/cloudstorage/dao/UserMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.cloudstorage.entity.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
