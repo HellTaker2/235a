09f25a7de55232d25580d2133aadc9848b1d4aba
YangRRhhhh <2811638663@qq.com>
2025-05-20 21:25:13 +0800

    删除了收藏文件的xml文件

--- src/main/java/com/example/cloudstorage/dao/FavouriteMapper.java
+++ src/main/java/com/example/cloudstorage/dao/FavouriteMapper.java
@@ -2,13 +2,22 @@ package com.example.cloudstorage.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.cloudstorage.entity.Favourite;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.dao.DuplicateKeyException;
+
 @Mapper
 public interface FavouriteMapper extends BaseMapper<Favourite> {
 
-    int insertFavorite(@Param("fid") String fid, @Param("uid") int uid);
+    @Insert("INSERT INTO favorite (FID, UID) VALUES (#{fid}, #{uid})")
+    int insertFavorite(@Param("fid") String fid, @Param("uid") int uid) throws DuplicateKeyException;
+
+    @Select("SELECT COUNT(*) FROM file WHERE FileID = #{fid} AND UID = #{uid}")
     int checkFileExists(@Param("fid") String fid, @Param("uid") int uid);
+
+    @Select("SELECT COUNT(*) FROM folder WHERE FolderID = #{fid} AND UID = #{uid}")
     int checkFolderExists(@Param("fid") String fid, @Param("uid") int uid);
 
 }
